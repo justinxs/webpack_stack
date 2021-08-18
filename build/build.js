25218ff2eff2bef623bc8f6956807230fbc9dfac
justin <978336184@qq.com>
2021-08-18 16:21:54 +0800

    修改

--- build/build.js
+++ build/build.js
@@ -32,12 +32,6 @@ module.exports = function build(webpackConfig, namesapce, distPath) {
             }
     
             console.log(chalk.cyan(' Build complete.\n'))
-            console.log(
-                chalk.yellow(
-                    ' Tip: built files are meant to be served over an HTTP server.\n' +
-                    " Opening index.html over file:// won't work.\n"
-                )
-            )
         })
     })
 }
