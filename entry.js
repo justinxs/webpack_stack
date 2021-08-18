ec6124d59b9258e4bdbc05697286734c71b39179
justin <978336184@qq.com>
2021-08-18 18:33:10 +0800

    修改

---
+++ entry.js
@@ -0,0 +1,11 @@
+module.exports = {
+    entry: './path/to/my/entry/file.js',
+    entry: {
+        main: './path/to/my/entry/file.js',
+    },
+    entry: {
+        pageOne: './src/pageOne/index.js',
+        pageTwo: './src/pageTwo/index.js',
+        pageThree: './src/pageThree/index.js',
+    },
+};
--- hello_webpack/webpack.config.js
+++ hello_webpack/webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
     mode: 'production',
@@ -9,5 +12,29 @@ module.exports = {
         filename: 'bundle.js',
         // 输出文件都放到 dist 目录下
         path: path.resolve(__dirname, './dist'),
-    }
+    },
+    optimization: {
+        minimize: false,
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+            }),
+            new CssMinimizerPlugin()
+        ],
+    },
+    module: {
+        rules: [
+            {
+                // 用正则去匹配要用该 loader 转换的 CSS 文件
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // 从 .js 文件中提取出来的 .css 文件的名称
+            filename: `[name]_[contenthash:8].css`,
+        }),
+    ]
 };
