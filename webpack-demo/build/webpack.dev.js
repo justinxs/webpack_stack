0c81720226b954bd09d6cffb5bacae852462e3d6
justin <978336184@qq.com>
2021-09-30 18:15:57 +0800

    add comment

--- webpack-demo/build/webpack.dev.js
+++ webpack-demo/build/webpack.dev.js
@@ -4,6 +4,7 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     // 从 webpack v4 开始, 指定 mode 会自动地配置 process.env.NODE_ENV
+    // 如果 mode 未通过配置或 CLI 赋值，CLI 将使用可能有效的 NODE_ENV 值作为 mode
     mode: 'development',
     devtool: 'cheap-source-map',
     // webpack-dev-server 和 webpack-dev-middleware 里 Watch 模式默认开启
--- webpack-demo/build/webpack.prod.js
+++ webpack-demo/build/webpack.prod.js
@@ -5,6 +5,7 @@ const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
     // 从 webpack v4 开始, 指定 mode 会自动地配置 process.env.NODE_ENV
+    // 如果 mode 未通过配置或 CLI 赋值，CLI 将使用可能有效的 NODE_ENV 值作为 mode
     // 自动开启 tree shaking 没被引用或引入不被使用过的 module 不会打包进来
     mode: 'production',
     optimization: {
